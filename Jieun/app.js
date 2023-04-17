b68fb7e62d67f3f364661a94c20ba1cf1b82c7c9
Geun9 <gsilverg197@gmail.com>
2023-04-17 21:06:10 +0900

    [ADD]: Express Initial Setting

---
+++ Jieun/app.js
@@ -0,0 +1,39 @@
+require("dotenv").config(); //환경변수 등록
+
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+const { DataSource } = require('typeorm');
+
+const dataSource = new DataSource({
+    type: process.env.DB_CONNECTION,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+})
+
+dataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+
+const app = express(); 
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+app.get('/ping', function (req, res, next) {
+    res.status(200).json({message: 'pong'})
+})
+
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, function() {
+    console.log(`server listening on port ${PORT}`)
+})
