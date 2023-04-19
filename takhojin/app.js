16b465f0acc95148cdcf498a6c3db1481f84ff04
탁호진 <sxsxkkjp@gmail.com>
2023-04-19 15:43:13 +0900

    수정

--- takhojin/app.js
+++ takhojin/app.js
@@ -3,14 +3,28 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const { DataSource } = require("typeorm");
+
+dotenv.config();
+
+const myDataSource = new DataSource({
+  type: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+myDataSource.initialize().then(() => {
+  console.log("Data Source has been initialized");
+});
 
 app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan("combined"));
-
-dotenv.config();
 
 app.get("/ping", (req, res) => {
   res.json({ message: " pong " });
