84c5dbc184a8a933e550a77018ca01832c69312e
탁호진 <sxsxkkjp@gmail.com>
2023-04-20 09:59:26 +0900

    2차 수정

--- takhojin/app.js
+++ takhojin/app.js
@@ -1,4 +1,3 @@
-const http = require("http");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
@@ -7,7 +6,7 @@ const { DataSource } = require("typeorm");
 
 dotenv.config();
 
-const myDataSource = new DataSource({
+const dataSource = new DataSource({
   type: process.env.DB_CONNECTION,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
@@ -16,25 +15,27 @@ const myDataSource = new DataSource({
   database: process.env.DB_DATABASE,
 });
 
-myDataSource.initialize().then(() => {
-  console.log("Data Source has been initialized");
-});
+dataSource
+  .initialize()
+  .then(() => {
+    console.log("Data Source has been initialized");
+  })
+  .catch((error) => console.log(error));
 
-app = express();
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan("combined"));
 
 app.get("/ping", (req, res) => {
-  res.json({ message: " pong " });
+  return res.status(200).json({ message: " pong " });
 });
 
-const server = http.createServer(app);
 const PORT = process.env.PORT;
 
 const start = async () => {
-  server.listen(PORT, () => console.log(`server is listening on ${PORT}`));
+  app.listen(PORT, () => console.log(`server is listening on ${PORT}`));
 };
 
 start();
