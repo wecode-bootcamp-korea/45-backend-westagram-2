eccbed42327913ea5a9218c585454eb6d6ed37f2
Inseok Chang <inni.chang@gmail.com>
2023-04-24 19:57:38 +0900

    Assignment 9: Layered Pattern [수정]

--- Inseok_45/models/likeDao.js
+++ Inseok_45/models/likeDao.js
@@ -1,22 +1,4 @@
-const { DataSource } = require('typeorm');
-
-const dataSource = new DataSource({
-    type: process.env.TYPEORM_CONNECTION,
-    host: process.env.TYPEORM_HOST,
-    port: process.env.TYPEORM_PORT,
-    username: process.env.TYPEORM_USERNAME,
-    password: process.env.TYPEORM_PASSWORD,
-    database: process.env.TYPEORM_DATABASE
-});
-
-dataSource
-    .initialize()
-    .then(() => {
-        console.log("Data source has been initialized!");
-    }).catch((err) => {
-        console.log("Data source Not Initialize : ", err);
-            dataSource.destroy();
-    });
+const dataSource  = require('./dataSource');
 
 const checkLike = async ( userId, postId ) => {
     try {
