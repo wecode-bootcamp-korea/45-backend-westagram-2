6d8a3fae11e36bcd3a8e2db26a4075201bbda10a
Geun9 <gsilverg197@gmail.com>
2023-04-24 12:00:25 +0900

    [MODIFY]: <Assignment8 좋아요 누르기> 좋아요 데이터 POST시, 이미 존재하는 데이터면 삭제하기

--- Jieun/controllers/likeController.js
+++ Jieun/controllers/likeController.js
@@ -4,8 +4,10 @@ const createLikes = async (req, res) => {
   try {
     const { userId, postId } = req.body;
 
-    await likeService.createLikes(userId, postId);
-    return res.status(201).json({ message: "likeCreated" });
+    const createLike = await likeService.createLikes(userId, postId);
+    if (createLike.insertId)
+      return res.status(201).json({ message: "likeCreated" });
+    else return res.status(200).json({ message: "likeDelete" });
   } catch (err) {
     console.log(err);
     return res.status(err.statusCode || 500).json({ message: err.message });
--- Jieun/models/likeDao.js
+++ Jieun/models/likeDao.js
@@ -1,5 +1,4 @@
 const { DataSource } = require("typeorm");
-
 const dataSource = new DataSource({
   type: process.env.DB_CONNECTION,
   host: process.env.DB_HOST,
@@ -8,7 +7,6 @@ const dataSource = new DataSource({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
-
 dataSource
   .initialize()
   .then(() => {
@@ -21,21 +19,45 @@ dataSource
 
 const createLikes = async (userId, postId) => {
   try {
-    return await dataSource.query(
-      `INSERT INTO likes(
-          user_id,
-          post_id
-      ) VALUES (
-          ?,
-          ?
-      )`,
+    const likeExists = await dataSource.query(
+      `SELECT EXISTS(
+        SELECT
+        user_id,
+        post_id
+        FROM likes
+        WHERE user_id = ? AND post_id = ?
+        )
+        `,
       [userId, postId]
     );
+    if (Object.values(likeExists[0]) == 0) {
+      const createLike = await dataSource.query(
+        `INSERT INTO likes(
+          user_id,
+          post_id
+          ) VALUES (
+            ?,
+            ?
+            )`,
+        [userId, postId]
+      );
+      return createLike;
+    } else {
+      const deleteLike = await dataSource.query(
+        `DELETE
+        FROM likes
+        WHERE user_id = ? AND post_id = ?;
+        `,
+        [userId, postId]
+      );
+      return deleteLike;
+    }
   } catch (err) {
     console.log("duplicateData: ", err);
     res.status(400).json({ message: "duplicateData" });
   }
 };
+
 module.exports = {
   createLikes,
 };
