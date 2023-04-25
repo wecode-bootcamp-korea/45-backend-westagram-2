d4105d9f39de84754571776cb8e267ec19fecb0e
탁호진 <sxsxkkjp@gmail.com>
2023-04-25 16:17:30 +0900

    modified

--- takhojin/services/userService.js
+++ takhojin/services/userService.js
@@ -1,23 +1,12 @@
 const userDao = require("../models/userDao");
+const { pwValidation } = require("../utils/validation-check");
 
 const signUp = async (email, password, description, profile_img) => {
-  const pwValidation = new RegExp(
-    "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})"
-  );
-  if (!pwValidation.test(password)) {
-    const err = new Error("PASSWORD_IS_NOT_VALID");
-    err.statusCode = 409;
-    throw err;
-  }
-  const createUser = await userDao.createUser(
-    email,
-    password,
-    description,
-    profile_img
-  );
+  await pwValidation(password);
+
+  const createUser = await userDao.createUser(email, description, profile_img);
   return createUser;
 };
-
 const searchUserPost = async (userId) => {
   const search = await userDao.searchUserPost(userId);
   return search;
