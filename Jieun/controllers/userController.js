f30808c1650c06717f5ee54aec24e0d08d5a0265
Geun9 <gsilverg197@gmail.com>
2023-04-25 22:51:45 +0900

    [MODIFY]: 메시지 수정

--- Jieun/controllers/userController.js
+++ Jieun/controllers/userController.js
@@ -20,7 +20,7 @@ const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const jwtToken = await userService.login(email, password);
-    return res.status(200).json({ message: jwtToken });
+    return res.status(200).json({ accessToken: jwtToken });
   } catch (err) {
     console.log(err);
     return res.status(err.statusCode || 500).json({ message: err.message });
