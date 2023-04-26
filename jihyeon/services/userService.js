4be775e620b9a5a6cc97a33dc2389c9da30b31b6
하지현 <hazzi@hajihyeon-ui-MacBookAir.local>
2023-04-26 14:07:20 +0900

    MODIFY

--- jihyeon/services/userService.js
+++ jihyeon/services/userService.js
@@ -1,17 +1,13 @@
-const createUserDao = require("../models/UserDao");
-const pwValidationCheck = require("../utils/validation-check.js");
 const bcrypt = require("bcrypt");
 
-const saltRounds = 12;
-
-const makeHash = async (password, saltRounds) => {
-  return await bcrypt.hash(password, saltRounds);
-};
+const createUserDao = require("../models/UserDao");
+const pwValidationCheck = require("../utils/validation-check.js");
 
 const signUp = async (email, password, name, age, phoneNumber) => {
   await pwValidationCheck(password);
 
-  const hashedPassword = await makeHash(password, saltRounds);
+  const hashedPassword = await bcrypt.hash(password, 12);
+
   return createUserDao.createUser(
     email,
     hashedPassword,
