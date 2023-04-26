6b1c784b39733d429e0acc7dcbcc21b54d38e687
하지현 <hazzi@hajihyeon-ui-MacBookAir.local>
2023-04-26 19:38:29 +0900

    MODIFY: authentication-authorization-3

--- jihyeon/middleware/auth.js
+++ jihyeon/middleware/auth.js
@@ -3,13 +3,13 @@ const jwt = require("jsonwebtoken");
 const validateToken = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
-    if (!token) return res.status(403).json("ACCESS_DENIED");
+    if (!token) return res.status(401).json("TOKEN_IS_EMPTY");
 
     const decoded = jwt.verify(token, process.env.SECRETKEY);
-    req.user = decoded.user_id;
+    req.user = decoded.userId;
     next();
   } catch (err) {
-    next(err);
+    return res.status(401).json("ACCESS_DENIED");
   }
 };
 
