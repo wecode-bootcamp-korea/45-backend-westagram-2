96582566b0ed5bdf9a5e241d8a1d9f17569ac687
Geun9 <gsilverg197@gmail.com>
2023-04-26 19:22:02 +0900

    [MODIFY]: error message 수정

--- Jieun/middlewares/auth.js
+++ Jieun/middlewares/auth.js
@@ -3,13 +3,14 @@ const jwt = require("jsonwebtoken");
 const auth = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
+    if (!token) return res.status(400).json({ message: "TOKEN_IS_EMPTY" });
     const payload = jwt.decode(token);
     const userId = payload.userId;
+    console.log("=========userId: ", userId);
     req.userId = userId;
     next();
   } catch (err) {
-    console.log(err);
-    return res.status(err.statusCode || 500).json({ message: err.message });
+    return res.status(err.statusCode || 401).json({ message: "TOKEN_CHECK" });
   }
 };
 
