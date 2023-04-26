40c60b6bbdaef87bd550c0d56729a77aca7acb8d
Geun9 <gsilverg197@gmail.com>
2023-04-26 19:29:57 +0900

    [MODIFY]: 공백 넣기

--- Jieun/middlewares/auth.js
+++ Jieun/middlewares/auth.js
@@ -3,7 +3,9 @@ const jwt = require("jsonwebtoken");
 const auth = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
+
     if (!token) return res.status(400).json({ message: "TOKEN_IS_EMPTY" });
+
     const payload = jwt.decode(token);
     req.userId = payload.userId;
     next();
